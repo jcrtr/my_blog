c0a8420739f1baf88861632b342a8f3d0ad8844e
likiblack <likiblack@gmail.com>
2020-11-12 09:29:31 +0300

    Update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import 'prismjs/themes/prism-okaidia.css';
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Navigation from "../components/navigation";
+import "prismjs/themes/prism-okaidia.css";
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -14,22 +14,36 @@ export default ({ children }) => {
         }
       }
     `
-  )
+  );
   return (
-    <div className="site-wrapper">
-      <header className="site-header">
-        <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
-        </div>
-        <Navigation />
-      </header>
-      {children}
+    <div className="content">
+      <div className="site-wrapper">
+        <header className="site-header">
+          <div className="site-title">
+            <Link to="/">{data.site.siteMetadata.title}</Link>
+          </div>
+          <Navigation />
+        </header>
+        {children}
+      </div>
       <footer className="site-footer">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#0099ff" fill-opacity="1" d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-</svg>
-        <p>&copy; {new Date().getFullYear()}&bull; <span role="img" aria-label="love">❤️</span> likiblack</p>
+          <path
+            fill="#381696"
+            fill-opacity="1"
+            d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+        <div className="content-footer">
+          <p>
+            &copy; {new Date().getFullYear()}&bull;{" "}
+            <span role="img" aria-label="love">
+              ❤️
+            </span>{" "}
+            likiblack
+          </p>
+        </div>
       </footer>
     </div>
-  )
-}
+  );
+};
