91aca504783a936c147687958f51550f884d1a44
John Carter <36883870+likiblack@users.noreply.github.com>
2020-11-12 09:01:55 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,10 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#0099ff" fill-opacity="1" d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+</svg>
+        <p>&copy; {new Date().getFullYear()}&bull; <span role="img" aria-label="love">❤️</span> likiblack</p>
       </footer>
     </div>
   )
